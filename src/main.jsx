ffbba8e2d1a9f25f4aa154333f0339c7023e461a
tvanmu <tresorvanmulders@gmail.com>
2026-05-22 09:07:06 +0200

    Self-host site fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/cormorant-garamond/latin-300.css";
+import "@fontsource/cormorant-garamond/latin-400-italic.css";
+import "@fontsource/space-mono/latin-400.css";
 import "./styles.css";
 import App from "./App.jsx";
 
